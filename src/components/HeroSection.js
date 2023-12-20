6ff23ea357d695e2de031b9a3c41ab6967e54fe1
Hamza Rehman <120000289+Hamza-Laghari@users.noreply.github.com>
2023-12-20 17:03:20 +0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,36 @@ import React from "react";
 import mockup from "../assets/img/Mockup.png";
 import { Image } from "react-bootstrap";
 import play from "../assets/img/Play 02.png";
+import Modal from "react-bootstrap/Modal";
+import video from "../assets/img/video.mp4";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-
-export default function HeroSection() {
+function MyVerticallyCenteredModal(props) {
   return (
-    <div div style={{ backgroundColor: "black" }}>
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <i>Web3Makr Trailer</i>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <video src={video} autoPlay="true"></video>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+export default function HeroSection(props) {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <div className="hero-bg">
       <div className="herosection">
         <div>
           <h1>
@@ -15,10 +40,16 @@ export default function HeroSection() {
           </h1>
           <p>
             Unlock the blockchain potential to create <br /> Web3 Applications
-          </p>
-          <Link to={"/"}>
-            <Image src={play} style={{ marginTop: "1rem" }} />
-          </Link>
+          </p>{" "}
+          <div>
+            <Link onClick={() => setModalShow(true)} rel="noopener noreferrer">
+              <Image src={play} style={{ marginTop: "1rem" }} />
+            </Link>
+            <MyVerticallyCenteredModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />{" "}
+          </div>
         </div>
       </div>
       <div
@@ -28,7 +59,7 @@ export default function HeroSection() {
           justifyContent: "center",
         }}
       >
-        <Image src={mockup} width={"900px"} />
+        <Image src={mockup} className="mockup" />
       </div>
     </div>
   );
